93fd29ca1d1a126e4771b221a9c350deabefb50e
maureeungaro <maurizio.ungaro@cnu.edu>
2016-12-13 09:05:48 -0500

    correct version

--- 3a.0.1/source/gemc.cc
+++ 3a.0.1/source/gemc.cc
@@ -29,7 +29,7 @@
 /// \author \n &copy; Maurizio Ungaro
 /// \author e-mail: ungaro@jlab.org\n\n\n
 
-const char *GEMC_VERSION = "gemc clas12 tag 3a.0.0";
+const char *GEMC_VERSION = "gemc clas12 tag 3a.0.1";
 
 // G4 headers
 #include "G4RunManager.hh"
