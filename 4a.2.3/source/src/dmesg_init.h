ac9422c2232e4174d78b4e8d26ddd16971b70b7f
ma <maurizio.ungaro@cnu.edu>
2020-09-08 12:40:00 -0400

    deleting stuff that wont be needed anymore

--- 4a.2.3/source/src/dmesg_init.h
+++
@@ -1,7 +0,0 @@
-#ifndef DMESG_H
-#define DMESG_H 1
-
-vector<string> init_dmesg(goptions);                  ///< General Initialization Routine
-vector<string> init_dvmesg(goptions, G4VisManager*);  ///< Initialization Routine for Visualization
-
-#endif
