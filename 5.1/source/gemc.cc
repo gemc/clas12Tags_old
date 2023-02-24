c21974874b34f3748b138aa85935a4912c410b0f
Maurizio Ungaro <ungaro@jlab.org>
2023-02-24 12:04:48 -0500

    optical id must still be 0 for 5.1

--- 5.1/source/gemc.cc
+++ 5.1/source/gemc.cc
@@ -417,7 +417,7 @@ int main( int argc, char **argv )
 
 // introducing OPTICALPHOTONPID here to be semi-transparent to G4 changes
 // this pid changed from 0 to -22 with geant4 10.7
-int MHit::OPTICALPHOTONPID = -22;
+int MHit::OPTICALPHOTONPID = 0;
 
 
 
